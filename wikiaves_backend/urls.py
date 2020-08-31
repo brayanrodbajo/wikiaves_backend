cf110c6df620140e1ded0cf0451de68ea6fcae43
Brayan Rodriguez <brayanrodbajo@gmail.com>
2020-08-31 15:53:43 -0500

    wikiaves prefix

--- wikiaves_backend/urls.py
+++ wikiaves_backend/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/users/',  include('users.urls')),
-    path('api/birds/',  include('birds.urls')),
+    path('wikiaves/admin/', admin.site.urls),
+    path('wikiaves/api/users/',  include('users.urls')),
+    path('wikiaves/api/birds/',  include('birds.urls')),
 ]
