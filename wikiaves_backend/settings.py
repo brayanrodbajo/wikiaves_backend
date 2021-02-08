01b00711b82639686ffed3d1874e07e08a9a13c3
Brayan Rodriguez <brayanrodbajo@gmail.com>
2021-02-08 15:19:52 -0500

    static_root

--- wikiaves_backend/settings.py
+++ wikiaves_backend/settings.py
@@ -174,6 +174,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # ALLAUTH
 ACCOUNT_USER_MODEL_USERNAME_FIELD = 'username'
